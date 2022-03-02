db38ddce7ccd43447dc010c9a2aaa7f37d2b091b
anmolm96 <anmol.maini@gmail.com>
2022-03-01 20:26:49 -0600

    Modal outline

--- src/views/TransactionView/index.tsx
+++ src/views/TransactionView/index.tsx
@@ -14,7 +14,22 @@ const transactions = [
   { name: "Desk", id: "3", status: "denied" },
 ];
 
+const Modal = ({ open, close }: { open: boolean; close(): void }) => (
+  <div className={`modal ${open && "modal-open"}`}>
+    <div className="modal-box">
+      <h3 className="font-bold text-lg">Add a new expense</h3>
+      <div className="flex gap-2 mt-4 justify-center">
+        <button className="btn btn-primary">Create</button>
+        <button className="btn" onClick={close}>
+          Close
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
 export const TransactionView: FC = ({}) => {
+  const [open, setOpen] = useState(false);
   const { connection } = useConnection();
   const [walletToParsePublicKey, setWalletToParsePublicKey] =
     useState<string>(walletPublicKey);
@@ -23,21 +38,23 @@ export const TransactionView: FC = ({}) => {
     <div className="container mx-auto max-w-6xl p-8 2xl:px-0">
       <div className={styles.container}>
         <Nav />
-
         <div className="text-center pt-2">
           <div className="hero min-h-16 p-0 pt-10">
             <div className="text-center hero-content w-full">
               <div className="w-full">
-                <h1 className="mb-5 text-5xl">Transactions for Anmol DAO</h1>
-
-                <div className="w-full min-w-full">
-                  <p className="mb-5">
-                    Here are some DAOs that use Slide to manage expenses
-                  </p>
+                <div className="flex justify-between">
+                  <h1 className="mb-5 text-5xl">Transactions for Anmol DAO</h1>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => setOpen(true)}
+                  >
+                    Create Expense
+                  </button>
                 </div>
-                <div className="my-10">
+                <div className="my-4">
                   <TransactionList transactions={transactions} />
                 </div>
+                <Modal open={open} close={() => setOpen(false)} />
               </div>
             </div>
           </div>
